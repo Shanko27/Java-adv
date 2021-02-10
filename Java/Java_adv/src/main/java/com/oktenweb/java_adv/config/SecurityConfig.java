d6b8ef918676acca3dae09268960515f9a93210c
Shanko27 <Shanko.i27@gmail.com>
2021-02-10 20:58:03 +0200

    adv1

--- Java/Java_adv/src/main/java/com/oktenweb/java_adv/config/SecurityConfig.java
+++ Java/Java_adv/src/main/java/com/oktenweb/java_adv/config/SecurityConfig.java
@@ -1,16 +1,13 @@
 package com.oktenweb.java_adv.config;
 
 import com.oktenweb.java_adv.service.IUserService;
-import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
